be699e487a1d533a57f531e3a3202efe80d8b6e7
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-07 19:24:10 +0300

    solved

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your solution here!
+// Define the variable
+const cats = ["Milo", "Otis", "Garfield"];
+//Define the Functions
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+module.exports = {
+    destructivelyAppendCat,
+    destructivelyPrependCat,
+
+};
+
